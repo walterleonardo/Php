c4c69fee786c33eda779fad51b841c7240b2c359
walter leonardo <walterleonardo@gmail.com>
2016-03-23 22:42:28 +0100

    ################## Last version fixed:6 Date: Autor: Walter Leonardo eMail: walterleonardo@gmail.com Info:
    Bug Info:
    
    
    Solution:
    
    ###################

--- login.wadisa.com/home/OLD/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start(); 
-require "../config/config.php"; 
-session_destroy(); 
-//echo "You have successfully logged out. <a href='login.php'> Click here </a> to login!"; 
-header ("location: ../login/index.php");
-?>
