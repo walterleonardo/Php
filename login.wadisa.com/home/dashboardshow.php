4c87fefa2b1a9d6cb71d05f0a0ba0957eab27771
walter leonardo <walterleonardo@gmail.com>
2016-04-07 13:15:06 +0200

    ################## Last version fixed: Date: Autor: Walter Leonardo eMail: walterleonardo@gmail.com Info:
    Bug Info:
    
    
    Solution:
    
    ###################

--- login.wadisa.com/home/dashboardshow.php
+++ login.wadisa.com/home/dashboardshow.php
@@ -281,7 +281,7 @@ if (isset($_GET['btn-update'])) {
             <script src="../js/bootstrap.min.js"></script>
 
             <!-- Plugin JavaScript -->
-            <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+            <script src="../js/jquery.easing.min.js"></script>
             <script src="../js/classie.js"></script>
             <script src="../js/cbpAnimatedHeader.js"></script>
 
--- login.wadisa.com/index.php
+++ login.wadisa.com/index.php
@@ -136,7 +136,7 @@ $empresa='QR';
         <script src="js/bootstrap.min.js"></script>
 
         <!-- Plugin JavaScript -->
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+        <script src="js/jquery.easing.min.js"></script>
         <script src="js/classie.js"></script>
         <script src="js/cbpAnimatedHeader.js"></script>
 
